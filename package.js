950cc0277bedda50b790717afab297f449858d33
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-04-16 12:14:37 -0700

    Update to Meteor 1.3

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'xolvio:backdoor',
   summary: 'Runs arbitrary code on server',
-  version: '0.2.0-rc.0',
+  version: '0.2.0',
   debugOnly: true
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.3-beta.11');
+  api.versionsFrom('1.3');
   api.use('ecmascript', 'server');
   api.use('check', 'server');
   api.use('modules', 'server');
@@ -14,10 +14,11 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function(api) {
-  api.versionsFrom('METEOR@1.3-beta.11');
+  api.versionsFrom('1.3');
   api.use('xolvio:backdoor');
   api.use('ecmascript');
-  api.use('sanjo:jasmine@0.20.3');
+  api.use('sanjo:jasmine@1.0.1');
+  api.use('velocity:html-reporter@0.10.0');
 
   api.addFiles('tests/xolvio_backdoor.js', 'client');
 });
